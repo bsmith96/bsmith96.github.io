9991dd81b416bc398c24ec53fffec855edc91b47
Ben Smith <ben@bensmithsound.uk>
2021-03-28 11:59:50 +0100

    Rewritten using jQuery
    Makes it slightly easier to read

--- newShowGen/script.js
+++ newShowGen/script.js
@@ -58,101 +58,101 @@ if (prodQuotes != []) {
 
 document.getElementById("test").innerHTML = htmlBlock;*/
 
-window.onload = function() {
-  document.getElementById("prodForm").onsubmit = function() {
-    var prodName = document.getElementById("prodName").value;
-    var prodMonth = document.getElementById("prodMonth").value;
-    var prodYear = document.getElementById("prodYear").value;
-    var prodDateString = document.getElementById("prodDateString").value;
-    var myRole = document.getElementById("myRole").value;
-    var prodVenue = document.getElementById("prodVenue").value;
-    var prodPoster = document.getElementById("prodPoster").value;
-    var prodProducer = document.getElementById("prodProducer").value;
-    var prodDirector = document.getElementById("prodDirector").value;
-    var prodSoundDesigner = document.getElementById("prodSoundDesigner").value;
-    var prodWritingCredits = document.getElementById("prodWritingCredits").value;
-    var prodDescription = document.getElementById("prodDescription").value;
-    var prodVideo = document.getElementById("prodVideo").value;
-    var prodQuote1 = document.getElementById("prodQuote1").value;
-    var prodSource1 = document.getElementById("prodSource1").value;
-    var prodQuoteTilt1 = document.getElementById("sticky-rotation-1").value;
-    var prodQuote2 = document.getElementById("prodQuote2").value;
-    var prodSource2 = document.getElementById("prodSource2").value;
-    var prodQuoteTilt2 = document.getElementById("sticky-rotation-2").value;
+
+$("#prodForm").submit(function(event) {
+  var prodName = $("#prodName").val();
+  var prodMonth = $("#prodMonth").val();
+  var prodYear = $("#prodYear").val();
+  var prodDateString = $("#prodDateString").val();
+  var myRole = $("#myRole").val();
+  var prodVenue = $("#prodVenue").val();
+  var prodPoster = $("#prodPoster").val();
+  var prodProducer = $("#prodProducer").val();
+  var prodDirector = $("#prodDirector").val();
+  var prodSoundDesigner = $("#prodSoundDesigner").val();
+  var prodWritingCredits = $("#prodWritingCredits").val();
+  var prodDescription = $("#prodDescription").val();
+  var prodVideo = $("#prodVideo").val();
+  var prodQuote1 = $("#prodQuote1").val();
+  var prodSource1 = $("#prodSource1").val();
+  var prodQuoteTilt1 = $("#sticky-rotation-1").val();
+  var prodQuote2 = $("#prodQuote2").val();
+  var prodSource2 = $("#prodSource2").val();
+  var prodQuoteTilt2 = $("#sticky-rotation-2").val();
 
 
-    // CALCULATED VARIABLES
+  // CALCULATED VARIABLES
 
-    var sectionComment = "<!--" + prodName + " - " + prodYear + "-->";
-    console.log(sectionComment);
-    var sectionId = yearTo2(prodYear) + prodMonth + removeSpaceAndLowercase(prodName);
-    console.log(sectionId);
-    var posterAltText = prodName + " show poster";
-    console.log(posterAltText);
-    if (prodProducer != "") {
-      var prodProducer = "<br>" + prodProducer;
-    }
-    var directorCredit = "Directed by " + prodDirector;
-    console.log(directorCredit);
-    var soundDesignerCredit = "Sound Design by " + prodSoundDesigner;
-    console.log(soundDesignerCredit);
-    var collapseId = removeSpaceVarCase(prodName) + "Desc";
-    console.log(collapseId);
-    var writingArray = prodWritingCredits.split("\n");
-    console.log(writingArray);
-    var writingCreditsFormatted = writingArray.join("<br>");
-    console.log(writingCreditsFormatted);
-    var descriptionArray = prodDescription.split("\n");
-    console.log(descriptionArray);
-    var firstDescription = descriptionArray[0];
-    console.log(firstDescription);
-    descriptionArray.shift();
-    console.log(descriptionArray);
-    var videoIframeSplit = prodVideo.split(">");
-    console.log(videoIframeSplit);
-    var videoIframe = videoIframeSplit[0] + " class=\"embedded-video\">" + videoIframeSplit[1] + ">";
-    console.log(videoIframe);
-    var prodQuotes = [];
-    prodQuotes.push(prodQuote1);
-    prodQuotes.push(prodQuote2);
-    var prodSources = [];
-    prodSources.push(prodSource1);
-    prodSources.push(prodSource2);
-    var prodQuoteTilts = [];
-    prodQuoteTilts.push(prodQuoteTilt1);
-    prodQuoteTilts.push(prodQuoteTilt2);
-    console.log(prodQuoteTilts);
-    var quoteBlocks = getQuotes(prodQuotes, prodSources, prodQuoteTilts);
-    console.log(quoteBlocks);
+  var sectionComment = "<!--" + prodName + " - " + prodYear + "-->";
+  console.log(sectionComment);
+  var sectionId = yearTo2(prodYear) + prodMonth + removeSpaceAndLowercase(prodName);
+  console.log(sectionId);
+  var posterAltText = prodName + " show poster";
+  console.log(posterAltText);
+  if (prodProducer != "") {
+    var prodProducer = "<br>" + prodProducer;
+  }
+  var directorCredit = "Directed by " + prodDirector;
+  console.log(directorCredit);
+  var soundDesignerCredit = "Sound Design by " + prodSoundDesigner;
+  console.log(soundDesignerCredit);
+  var collapseId = removeSpaceVarCase(prodName) + "Desc";
+  console.log(collapseId);
+  var writingArray = prodWritingCredits.split("\n");
+  console.log(writingArray);
+  var writingCreditsFormatted = writingArray.join("<br>");
+  console.log(writingCreditsFormatted);
+  var descriptionArray = prodDescription.split("\n");
+  console.log(descriptionArray);
+  var firstDescription = descriptionArray[0];
+  console.log(firstDescription);
+  descriptionArray.shift();
+  console.log(descriptionArray);
+  var videoIframeSplit = prodVideo.split(">");
+  console.log(videoIframeSplit);
+  var videoIframe = videoIframeSplit[0] + " class=\"embedded-video\">" + videoIframeSplit[1] + ">";
+  console.log(videoIframe);
+  var prodQuotes = [];
+  prodQuotes.push(prodQuote1);
+  prodQuotes.push(prodQuote2);
+  var prodSources = [];
+  prodSources.push(prodSource1);
+  prodSources.push(prodSource2);
+  var prodQuoteTilts = [];
+  prodQuoteTilts.push(prodQuoteTilt1);
+  prodQuoteTilts.push(prodQuoteTilt2);
+  console.log(prodQuoteTilts);
+  var quoteBlocks = getQuotes(prodQuotes, prodSources, prodQuoteTilts);
+  console.log(quoteBlocks);
 
-    // GENERATED HTML BLOCK
+  // GENERATED HTML BLOCK
 
-    var htmlBlock = sectionComment;
-    htmlBlock = htmlBlock + "\n    <div id=\"" + sectionId + "\" class=\"container-fluid px-4 mt-3 show-info\">\n      <div class=\"row gx-5 align-items-center position-relative\">\n        <div class=\"col-md-3 gy-4\">\n          <figure class=\"figure\">\n            <img src=\"./images/show-posters/" + prodPoster + "\" class=\"figure-img img-fluid rounded shadow\" alt=\"" + posterAltText + "\">\n          </figure>\n        </div>\n        <div class=\"col-sm gy-3\">\n          <p class=\"show-info-name\">" + prodName + "</p>\n          <p class=\"show-info-role\"><strong>" + myRole + "</strong> | " + prodVenue + prodProducer + "</p>\n          <p class=\"show-info-creatives\">" + directorCredit + "<br>" + soundDesignerCredit + "</p>\n          <p class=\"show-info-readmore\">\n            <a data-bs-toggle=\"collapse\" href=\"#" + collapseId + "\" role=\"button\" aria-expanded=\"false\" aria-controls=\"" + collapseId + "\" class=\"stretched-link\">\n              Read more...\n            </a>\n          </p>\n        </div>\n        <div class=\"col-sm-2\">\n          <p class=\"show-info-date\">" + prodDateString + "</p>\n        </div>\n      </div>\n      <div class=\"collapse\" id=\"" + collapseId + "\" data-bs-parent=\"#all-projects\">\n        <div class=\"row justify-content-center pt-3\">\n          <div class=\"col-md-3\"></div>\n          <div class=\"col-md-6 mb-2 show-info-desc\">\n            <p class=\"show-info-creatives\">" + writingCreditsFormatted + "</p>\n            <p class=\"lead\">" + firstDescription + "</p>\n            <p>" + descriptionArray.join("</p>\n            <p>") + "</p>\n          </div>\n          <div class=\"col-md-3 mb-2 show-info-sidebar\">";
+  var htmlBlock = sectionComment;
+  htmlBlock = htmlBlock + "\n    <div id=\"" + sectionId + "\" class=\"container-fluid px-4 mt-3 show-info\">\n      <div class=\"row gx-5 align-items-center position-relative\">\n        <div class=\"col-md-3 gy-4\">\n          <figure class=\"figure\">\n            <img src=\"./images/show-posters/" + prodPoster + "\" class=\"figure-img img-fluid rounded shadow\" alt=\"" + posterAltText + "\">\n          </figure>\n        </div>\n        <div class=\"col-sm gy-3\">\n          <p class=\"show-info-name\">" + prodName + "</p>\n          <p class=\"show-info-role\"><strong>" + myRole + "</strong> | " + prodVenue + prodProducer + "</p>\n          <p class=\"show-info-creatives\">" + directorCredit + "<br>" + soundDesignerCredit + "</p>\n          <p class=\"show-info-readmore\">\n            <a data-bs-toggle=\"collapse\" href=\"#" + collapseId + "\" role=\"button\" aria-expanded=\"false\" aria-controls=\"" + collapseId + "\" class=\"stretched-link\">\n              Read more...\n            </a>\n          </p>\n        </div>\n        <div class=\"col-sm-2\">\n          <p class=\"show-info-date\">" + prodDateString + "</p>\n        </div>\n      </div>\n      <div class=\"collapse\" id=\"" + collapseId + "\" data-bs-parent=\"#all-projects\">\n        <div class=\"row justify-content-center pt-3\">\n          <div class=\"col-md-3\"></div>\n          <div class=\"col-md-6 mb-2 show-info-desc\">\n            <p class=\"show-info-creatives\">" + writingCreditsFormatted + "</p>\n            <p class=\"lead\">" + firstDescription + "</p>\n            <p>" + descriptionArray.join("</p>\n            <p>") + "</p>\n          </div>\n          <div class=\"col-md-3 mb-2 show-info-sidebar\">";
 
-    //document.getElementById("test").innerHTML = htmlBlock;
+  //document.getElementById("test").innerHTML = htmlBlock;
 
-    if (prodVideo != "") {
-      htmlBlock = htmlBlock + "\n            <div class=\"ratio ratio-16x9 mt-5 mb-2\">\n            " + videoIframe + "\n          </div>"
-    }
+  if (prodVideo != "") {
+    htmlBlock = htmlBlock + "\n            <div class=\"ratio ratio-16x9 mt-5 mb-2\">\n            " + videoIframe + "\n          </div>"
+  }
 
-    if (prodQuotes != [""]) {
-      htmlBlock = htmlBlock + "\n            " + quoteBlocks;
-    }
+  if (prodQuotes != [""]) {
+    htmlBlock = htmlBlock + "\n            " + quoteBlocks;
+  }
 
-    htmlBlock = htmlBlock + "\n          </div>\n        </div>\n      </div>\n    </div>";
+  htmlBlock = htmlBlock + "\n          </div>\n        </div>\n      </div>\n    </div>";
 
-    document.getElementById("output").value = htmlBlock;
-    document.getElementById("outputVisual").innerHTML = htmlBlock;
-    event.preventDefault();
-  };
-};
+  document.getElementById("output").value = htmlBlock;
+  document.getElementById("outputVisual").innerHTML = htmlBlock;
+  event.preventDefault();
+})
+
 
 // Copy HTML
-document.getElementById("copyhtml").onclick = function(){
-  document.getElementById("output").select();
+$("#copyhtml").click(function(){
+  $("#output").select();
   document.execCommand('copy');
-}
+})
 
 
 /*
